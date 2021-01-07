a44a0cd760ed55e6373b5ee6076c123c46316aab
jz991724 <1548293573@qq.com>
2021-01-07 17:57:02 +0800

    ADD MultipleChoiceBox组件页面

---
+++ src/components/select/MultipleChoiceBox.vue
@@ -0,0 +1,22 @@
+/**
+* @描述: 多选框
+* @作者: 张俊
+* @创建时间: 2021-01-07 17:55:14
+*/
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({ name: 'MultipleChoiceBox' })
+export default class MultipleChoiceBox extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
