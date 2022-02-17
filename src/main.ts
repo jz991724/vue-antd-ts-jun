49175c743d1e330a68de2066771a64720ae300fe
jz991724 <1548293573@qq.com>
2022-02-17 17:18:03 +0800

    add 语法检查

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import Vue from 'vue';
+import Antd from 'ant-design-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
+
 Vue.use(Antd);
 
 Vue.config.productionTip = false;
